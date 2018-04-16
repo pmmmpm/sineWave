c308f02f1e3cb105164ab9b5941333c1ecfaa289
Philipp Möckli <philipp@debutdebut.com>
2018-04-16 19:35:23 +0200

    3

--- sketch.js
+++ sketch.js
@@ -1,12 +1,35 @@
 let cnv;
+let radius = 20;
+let vertices = 3;
 
 function setup() {
 	cnv = createCanvas(innerWidth, innerHeight);
-	cnv.parent('p5Sketch')
+	cnv.parent('p5Sketch');
 }
 
 function draw() {
 	background(51);
+	
+	let value = document.getElementById('vertices').value;
+	// console.log(value);
+	vertices = value;
+	for(let i = 0; i < 10; i++ ){
+		polygon(vertices, radius * i, width/2, height/2);
+	}
+
+}
+
+function polygon(vert, r, posX ,posY){
+	beginShape();
+	noFill();
+	stroke(153,153,0);
+	for(let i = 0; i < vert; i++){
+		let angle = map(i, 0 , vert, 0, TWO_PI);
+		let x = cos(angle) * r + posX;
+		let y = sin(angle) * (r / 2) + posY;
+		vertex(x, y);
+	}
+	endShape(CLOSE);
 }
 
 function windowResized(){
