abab7d9ec8006914bcd8540d858a5290bc4265e4
Philipp Möckli <philipp@debutdebut.com>
2018-04-16 20:07:54 +0200

    5

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let cnv;
 // let radius = 20;
 // let vertices = 3;
+let strovalue = 1;
 
 function setup() {
 	cnv = createCanvas(innerWidth, innerHeight);
@@ -12,8 +13,10 @@ function draw() {
 	
 	let vervalue = document.getElementById('vertices').value;
 	let radvalue = document.getElementById('radius').value;
+	strovalue = document.getElementById('strokeWeight').value;
 	vertices = vervalue;
 	radius = radvalue;
+	// console.log(strovalue);
 	for(let i = 0; i < 10; i++ ){
 		polygon(vertices, radius * i, width/2, height/2);
 	}
@@ -24,11 +27,12 @@ function draw() {
 function polygon(vert, r, posX ,posY){
 	beginShape();
 	noFill();
-	stroke(153,153,0);
+	stroke(255,204,229);
+	strokeWeight(strovalue);
 	for(let i = 0; i < vert; i++){
 		let angle = map(i, 0 , vert, 0, TWO_PI);
 		let x = cos(angle) * r + posX;
-		let y = sin(angle) * (r / 2) + posY;
+		let y = sin(angle) * (r/2) + posY;
 		vertex(x, y);
 	}
 	endShape(CLOSE);
