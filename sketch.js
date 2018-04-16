3e1db9cf5a10ca0f383aa70ef3f5b093681e0922
Philipp Möckli <philipp@debutdebut.com>
2018-04-16 20:30:18 +0200

    6

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
 let cnv;
 // let radius = 20;
 // let vertices = 3;
-let strovalue = 1;
+// let strovalue = 1;
+let inc;
+let wave = 0;
+let waveHeight = 20;
+let waveFreq = 0;
+let period = 2;
 
 function setup() {
 	cnv = createCanvas(innerWidth, innerHeight);
 	cnv.parent('p5Sketch');
+	inc = PI / 40;
 }
 
 function draw() {
@@ -13,14 +19,17 @@ function draw() {
 	
 	let vervalue = document.getElementById('vertices').value;
 	let radvalue = document.getElementById('radius').value;
-	strovalue = document.getElementById('strokeWeight').value;
+	// strovalue = document.getElementById('strokeWeight').value;
 	vertices = vervalue;
 	radius = radvalue;
 	// console.log(strovalue);
 	for(let i = 0; i < 10; i++ ){
-		polygon(vertices, radius * i, width/2, height/2);
+		waveFreq = map(i,0, 10, 0, TWO_PI * period);
+		let y = (height/2) + (sin(wave+waveFreq) * waveHeight);
+		polygon(vertices, radius * i, width/2, y);
 	}
 	
+	wave += inc;
 
 }
 
@@ -28,7 +37,8 @@ function polygon(vert, r, posX ,posY){
 	beginShape();
 	noFill();
 	stroke(255,204,229);
-	strokeWeight(strovalue);
+	// strokeWeight(strovalue);
+	strokeWeight((sin(wave+waveFreq) * waveHeight)*1);
 	for(let i = 0; i < vert; i++){
 		let angle = map(i, 0 , vert, 0, TWO_PI);
 		let x = cos(angle) * r + posX;
